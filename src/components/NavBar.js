b2541a3a3f7e278409c74b42dc58673491fad9e6
Umang Garg <umang@Umangs-MacBook-Pro.local>
2022-01-02 01:06:54 -0500

    resume pdf -

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -72,11 +72,11 @@ export default function NavBar({ navColor, navSize }) {
               className="pri-font whittish inline-flex item-center px-3 my-6 text-red-200"
             >CONTACT
             </HashLink>
-            <HashLink
+            {/* <HashLink
               activeClassName="text-red-100 nav_item_active bg-red-700"
               className="pri-font whittish inline-flex item-center px-3 my-6 text-red-200"
               to="/public/Resume.pdf" target="_blank" download>Resume
-            </HashLink>
+            </HashLink> */}
           </div>
         </nav>
       </div>
